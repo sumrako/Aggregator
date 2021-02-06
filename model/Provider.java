fba0ba864e39a1aafcfe34e36ce86dd2e511f1e8
Andrei Arnautov <aleksejtalitej@gmail.com>
2021-02-06 15:30:55 +0400

    Package commit

--- model/Provider.java
+++ model/Provider.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task28.task2810.model;
 
-import com.javarush.task.task28.task2810.Vacancy;
+import com.javarush.task.task28.task2810.vo.Vacancy;
 
 import java.util.List;
 
